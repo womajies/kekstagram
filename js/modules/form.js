40ee3277b66456d5232f0f95e9410cc83887f72e
Шамиль Магомедов <s.magomedov@m-games-ltd.com>
2021-12-13 18:03:38 +0300

    added handlers for effects, hashtags and comments

--- js/modules/form.js
+++ js/modules/form.js
@@ -4,50 +4,122 @@ const fileUpload = document.querySelector('#upload-file');
 const fileUploadOverlay = document.querySelector('.img-upload__overlay');
 const hashTagsInput = fileUploadOverlay.querySelector('.text__hashtags');
 const textDescription = fileUploadOverlay.querySelector('.text__description');
+const effectsForm = document.querySelector('.effects');
+const uploadImage = document.querySelector('.img-upload__preview img');
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlValue = document.querySelector('.scale__control--value');
+const MIN_SCALE_VALUE = 0;
+const MAX_SCALE_VALUE = 100;
 const re = /^#[A-Za-zА-Яа-я0-9]{1,19}$/;
 const MAX_HASHTAG_LENGTH = 20;
 const MIN_HASHTAG_LENGTH = 2;
 const MAX_HASHTAG_COUNT = 5;
 const MAX_COMMENT_LETTER = 140;
 
-const editImage = () => {
+const scaleSmaller = () => {
+  const scaleControlValueNumber = Number(scaleControlValue.value.slice(0, -1));
 
-};
-
-const closeUploadPopup = () => {
-  fileUploadOverlay.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  // eslint-disable-next-line no-use-before-define
-  document.removeEventListener('keydown', onUploadEscKeydown);
-  fileUpload.value = null;
-  hashTagsInput.value = null;
-  textDescription.value = null;
-  fileUploadOverlay.querySelector('#effect-none').checked = true;
-  inputErrorReset(hashTagsInput);
-  inputErrorReset(textDescription);
-};
-
-function onUploadEscKeydown (evt) {
-  if (evt.key === 'Escape' && !evt.target.matches('input[class="text__hashtags"]') && !evt.target.matches('textarea')) {
-    evt.preventDefault();
-    closeUploadPopup();
+  if (scaleControlValueNumber > MIN_SCALE_VALUE) {
+    scaleControlValue.value = `${scaleControlValueNumber - 25}%`;
+    uploadImage.style.transform = `scale(${(scaleControlValueNumber - 25) / MAX_SCALE_VALUE})`;
   }
-}
-
-const openUploadPopup = (picture) => {
-  fileUploadOverlay.classList.remove('hidden');
-  editImage(picture);
-
-  document.body.classList.add('modal-open');
-  document.querySelector('#upload-cancel').addEventListener('click', closeUploadPopup);
-  document.addEventListener('keydown', onUploadEscKeydown);
 };
 
-fileUpload.addEventListener('change', () => {
-  openUploadPopup();
-});
+const scaleBigger = () => {
+  const scaleControlValueNumber = Number(scaleControlValue.value.slice(0, -1));
 
-hashTagsInput.addEventListener('input', () => {
+  if (scaleControlValueNumber < MAX_SCALE_VALUE) {
+    scaleControlValue.value = `${scaleControlValueNumber + 25}%`;
+    uploadImage.style.transform = `scale(${(scaleControlValueNumber + 25) / MAX_SCALE_VALUE})`;
+  }
+};
+
+const effectsHandler = (evt) => {
+  const slider = document.querySelector('.effect-level__slider');
+  const sliderValue = document.querySelector('.effect-level__value');
+
+  uploadImage.removeAttribute('class');
+
+  if (!slider.noUiSlider) {
+    noUiSlider.create(slider, {
+      start: 1,
+      connect: 'lower',
+      range: {
+        'min': 0,
+        'max': 1,
+      },
+      step: 0.1,
+    });
+    slider.noUiSlider.on('update', (values, handle, unencoded) => {
+      sliderValue.value = `${unencoded[handle]}`;
+
+      if (uploadImage.classList.contains('effects__preview--chrome')) {
+        uploadImage.style.filter = `grayscale(${unencoded[handle]})`;
+      } else if (uploadImage.classList.contains('effects__preview--sepia')) {
+        uploadImage.style.filter = `sepia(${unencoded[handle]})`;
+      } else if (uploadImage.classList.contains('effects__preview--marvin')) {
+        uploadImage.style.filter = `invert(${unencoded[handle]}%)`;
+      } else if (uploadImage.classList.contains('effects__preview--phobos')) {
+        uploadImage.style.filter = `blur(${unencoded[handle]}px)`;
+      } else if (uploadImage.classList.contains('effects__preview--heat')) {
+        uploadImage.style.filter = `brightness(${unencoded[handle]})`;
+      }
+    });
+  }
+  if (evt.target.value !== 'none') {
+    uploadImage.classList.add(`effects__preview--${evt.target.value}`);
+
+    switch (evt.target.value) {
+      case 'chrome':
+      case 'sepia':
+        slider.noUiSlider.updateOptions({
+          start: 1,
+          step: 0.1,
+          range: {
+            'min': 0,
+            'max': 1,
+          },
+        });
+        break;
+      case 'marvin':
+        slider.noUiSlider.updateOptions({
+          start: 100,
+          step: 1,
+          range: {
+            'min': 0,
+            'max': 100,
+          },
+        });
+        break;
+      case 'phobos':
+        slider.noUiSlider.updateOptions({
+          start: 3,
+          step: 1,
+          range: {
+            'min': 0,
+            'max': 3,
+          },
+        });
+        break;
+      case 'heat':
+        slider.noUiSlider.updateOptions({
+          start: 3,
+          step: 0.1,
+          range: {
+            'min': 1,
+            'max': 3,
+          },
+        });
+        break;
+    }
+  } else {
+    slider.noUiSlider.destroy();
+    uploadImage.style.filter = 'none';
+  }
+};
+
+const hashTagsHandler = () => {
   const inputHashTagArray = hashTagsInput.value.trim().replace(/\s+/g, ' ').toLowerCase().split(' ');
 
   const checkHashTagsInput = () => {
@@ -85,9 +157,9 @@ hashTagsInput.addEventListener('input', () => {
   }
 
   hashTagsInput.reportValidity();
-});
+};
 
-textDescription.addEventListener('input', () => {
+const commentHandler = () => {
   if (textDescription.value.length > MAX_COMMENT_LETTER) {
     textDescription.setCustomValidity(
       `Превышена максимальная длина комментария. 
@@ -99,4 +171,47 @@ textDescription.addEventListener('input', () => {
   }
 
   textDescription.reportValidity();
-});
+};
+
+const closeUploadPopup = () => {
+  fileUploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  // eslint-disable-next-line no-use-before-define
+  document.removeEventListener('keydown', onUploadEscKeydown);
+  scaleControlSmaller.removeEventListener('click', scaleSmaller);
+  scaleControlBigger.removeEventListener('click', scaleBigger);
+  effectsForm.removeEventListener('change', effectsHandler);
+  hashTagsInput.removeEventListener('input', hashTagsHandler);
+  textDescription.removeEventListener('input', commentHandler);
+  uploadImage.removeAttribute('class');
+  uploadImage.style.filter = 'none';
+  fileUpload.value = null;
+  hashTagsInput.value = null;
+  textDescription.value = null;
+  fileUploadOverlay.querySelector('#effect-none').checked = true;
+  inputErrorReset(hashTagsInput);
+  inputErrorReset(textDescription);
+};
+
+function onUploadEscKeydown (evt) {
+  if (evt.key === 'Escape' && !evt.target.matches('input[class="text__hashtags"]') && !evt.target.matches('textarea')) {
+    evt.preventDefault();
+    closeUploadPopup();
+  }
+}
+
+const openUploadPopup = () => {
+  fileUploadOverlay.classList.remove('hidden');
+
+  document.body.classList.add('modal-open');
+  document.querySelector('#upload-cancel').addEventListener('click', closeUploadPopup);
+  document.addEventListener('keydown', onUploadEscKeydown);
+
+  scaleControlSmaller.addEventListener('click', scaleSmaller);
+  scaleControlBigger.addEventListener('click', scaleBigger);
+  effectsForm.addEventListener('change', effectsHandler);
+  hashTagsInput.addEventListener('input', hashTagsHandler);
+  textDescription.addEventListener('input', commentHandler);
+};
+
+fileUpload.addEventListener('change', openUploadPopup);
